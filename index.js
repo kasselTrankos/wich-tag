d0e68110cc0facf70df412f0cb015dccb6db3508
kasselTrankos <alvaro.touzon@gmail.com>
2019-10-01 23:07:53 +0200

    less code

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import {get} from './src/read';
 import { exec } from 'child_process';
 import decode from 'unescape';
-import fs from 'fs';
 
-const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
 const [id, dest = '.'] = process.argv.slice(2);
 var Left = function(x) {
   this.__value = x;
@@ -62,16 +60,14 @@ const getTitle = async id => {
     : Right.of(await get(url.__value));
   return prop('__value')(html.map(title).map(prop(1)).map(clean).map(decode));
 }
-const move = file => f => fs.rename(`./${file}`, `${dest}/${file}`, f);
 
 const init = async id => {
   const data = f => _exec => _exec.stdout.on('data', f);
-  const close = f => _exec => _exec.on('close', () => move(`${title}[${id}].mp3`)(f))
+  const close = f => _exec => _exec.on('close', f)
   const title = await getTitle(id);
-  console.log(title);
   const yParams = ['--extract-audio --audio-format', 'mp3',
   '--audio-quality', '190K',
-  '--output', `"${title}[%(id)s].%(ext)s"`,
+  '--output', `"${dest}/${title}[%(id)s].%(ext)s"`,
   '--add-metadata --metadata-from-title', `"%(artist)s - %(title)s %(track)s" ${id}`]
   const q = `youtube-dl ${yParams.join(' ')}`;
   const io_exec = new IO(exec(q));
